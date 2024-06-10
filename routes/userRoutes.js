f01c0ccb96f12967bda91fea34a77989c7ddda09
CobraBubbles89 <vi060489jjv@gmail.com>
2024-06-11 00:06:16 +0300

    realized userRoutes methods

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,75 @@ import { responseMiddleware } from "../middlewares/response.middleware.js";
 
 const router = Router();
 
-// TODO: Implement route controllers for user
+router.get('/', function (req, res, next) {
+    const users = userService.getAllUsers();
+    if (users) {
+        res.status(200).send(JSON.stringify(users));
+    } else {
+        const error = {
+            "error": true,
+            "message": "Users does not found!"
+        }
+        res.status(404).send(JSON.stringify(error));
+    }
+});
+
+router.get('/:id', (req, res) => {
+  const data = userService.getUserById(req.params.id);
+  console.log(data)
+    if (data) {
+        res.status(200).send(JSON.stringify(data));
+    } else {
+        const error = {
+            "error": true,
+            "message": "User does not found!"
+        };
+        res.status(404).send(JSON.stringify(error));
+    }
+})
+
+router.post('/', (req, res) => {
+  const createUser = userService.createUser(req.body);
+  if (createUser) {
+    res.status(200).send(JSON.stringify('Message: User created successfully!'))
+  } else {
+        const error = {
+            "error": true,
+            "message": "Sorry, user is not created.."
+        };
+        res.status(400).send(JSON.stringify(error));
+  }
+});
+
+router.put('/:id', function (req, res) {
+    const updateUser = userService.updateUser(req.params.id, req.body);
+    if (updateUser) {
+        res.status(200).send(JSON.stringify({
+            "message": "User update success"
+        }));
+    } else {
+        const error = {
+            "error": true,
+            "message": "Sorry, User does not updated!"
+        };
+        res.status(400).send(JSON.stringify(error));
+    }
+});
+
+router.delete('/:id', (req, res) => {
+      const user = userService.deleteUser(req.params.id);
+    if (user) {
+        res.status(200).send(JSON.stringify({
+            "message": "User deleted success"
+        }));
+    } else {
+        const error = {
+            "error": true,
+            "message": "Sorry, user does not deleted!"
+        };
+        res.status(400).send(JSON.stringify(error));
+    }
+});
+
 
 export { router };
--- services/userService.js
+++ services/userService.js
@@ -3,6 +3,39 @@ import { userRepository } from "../repositories/userRepository.js";
 class UserService {
   // TODO: Implement methods to work with user
 
+  getAllUsers() {
+    const users = userRepository.getAll();
+    if (!users) {
+      return null;
+    } else {
+      return users;
+    }
+  }
+
+  createUser(data) {
+    return userRepository.create(data);
+  }
+
+  updateUser(id, data) {
+    return userRepository.update(id, data);
+  }
+
+  deleteUser(id) {
+    const user = userRepository.delete(id);
+    if (!user) {
+        return null;
+    }
+    return user;
+  }
+  
+  getUserById(search) {
+    const user = userRepository.getOne(search);
+    if (!user) {
+        return null;
+    }
+    return user;
+}
+
   search(search) {
     const item = userRepository.getOne(search);
     if (!item) {
